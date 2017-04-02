20f407ac04c0adf6a22af0a5115506adc448c33f
Zohaib Nawaz Tarar <zohaib_nawaz786@hotmail.com>
2017-04-02 07:07:31 +0100

    new web

---
+++ wp-admin/customize.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://netseo.tk/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fcustomize.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://netseo.tk/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fcustomize.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="http://netseo.tk/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fcustomize.php&reauth=1">http://netseo.tk/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fwp-admin%2Fcustomize.php&reauth=1</a></p>
+	</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Test &lsaquo; Log In</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='http://netseo.tk/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='ninja-forms-display-css'  href='http://netseo.tk/wp-content/plugins/ninja-forms/deprecated/css/ninja-forms-display.css?nf_ver=2.9.58&#038;ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-qtip-css'  href='http://netseo.tk/wp-content/plugins/ninja-forms/deprecated/css/qtip.css?ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='jquery-rating-css'  href='http://netseo.tk/wp-content/plugins/ninja-forms/deprecated/css/jquery.rating.css?ver=4.7.3' type='text/css' media='all' />
+<link rel='stylesheet' id='mmm_mega_main_menu-css'  href='http://netseo.tk/wp-content/plugins/mega_main_menu/src/css/cache.skin.css?ver=1491108577' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Test</a></h1>
+	
+<form name="loginform" id="loginform" action="http://netseo.tk/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://netseo.tk/wp-admin/profile.php" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://netseo.tk/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://netseo.tk/">&larr; Back to Test</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
